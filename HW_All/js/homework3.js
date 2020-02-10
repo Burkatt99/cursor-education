2903a684186a633e99b4a40b978ed02fbe8aabcc
Burkatt99 <tanya.bura.99@gmail.com>
2020-02-10 17:52:24 +0200

    task3

--- HW_All/js/homework3.js
+++ HW_All/js/homework3.js
@@ -1,19 +1,10 @@
-let number = prompt("Write to number, please:");
-let wordForChange = prompt("Write word please:");
-let salary = prompt("Write please your salary!");
-let min = prompt("Write minimum number");
-let	max = prompt("Write maximum number");
-let wordWithRepeatLetter = prompt("write word for check").split('');
-let lengthPassword = prompt('Write to length for your password');
-let wordForDelete = prompt('Write please your word').split('');
-let wordPolyndrom = prompt('Write please your word for check').toLowerCase().replace(/ /g, '').split('');
 
 function getMaxDigit(number){
 	const elements = number.split('');
 	const maxNum = Math.max(...elements);
 	return maxNum;		
 }
-document.writeln(`<p>Result function #1: ${getMaxDigit()}.</p>`);
+document.writeln(`<p>Result function #1: ${getMaxDigit('5475')}.</p>`);
 
 function rowNumber(number, row){
 	if(row === 0){
@@ -30,31 +21,31 @@ document.writeln(`<p>Result function #2: ${rowNumber(2,5)}.</p>`);
 function changeLetter(wordForChange){
 	return wordForChange[0].toUpperCase() + wordForChange.slice(1).toLowerCase();		
 }
-document.writeln(`<p>Result function #3: ${changeLetter()}.</p>`);
+document.writeln(`<p>Result function #3: ${changeLetter('vLaD')}.</p>`);
 
 function salaryWithTax(salary){
 	const tax = 19.5;
 	const percent = 100;
 	return summa = (salary * (percent-tax))/percent;
 }
-document.writeln(`<p>Result function #4: ${salaryWithTax()}.</p>`);
+document.writeln(`<p>Result function #4: ${salaryWithTax(1000)}.</p>`);
 
 function randomNumber(min,max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Включаючи мінімум та максимум 
 }
-document.writeln(`<p>Result function #5: ${randomNumber()}.</p>`);
+document.writeln(`<p>Result function #5: ${randomNumber(1,10)}.</p>`);
 
-function repeatLetter(wordWithRepeatLetter){
-	const letter = prompt("Which letter need to check?");
+function repeatLetter(wordWithRepeatLetter,letter){
 	let n = 0;
+	wordWithRepeatLetter = wordWithRepeatLetter.toLowerCase();
 	for (let i = 0; i <= wordWithRepeatLetter.length; i++) {
-		if (letter === word[i] ) {
+		if (letter === wordWithRepeatLetter[i] ) {
 			n+=1;
 		}
 	}
 	return n;
 }
-document.writeln(`<p>Result function #6: ${repeatLetter()}.</p>`);
+document.writeln(`<p>Result function #6: ${repeatLetter('Ambasador', 'a')}.</p>`);
 
 /* не розпізнає символ
 let money = prompt('How much money do you have?').toLowerCase();
@@ -84,22 +75,23 @@ function password(lengthPassword){
 	}
 	return arrForNumber.join('');
 }
-document.writeln(`<p>Result function #8: ${password()}.</p>`);
+document.writeln(`<p>Result function #8: ${password(4)}.</p>`);
 
 
-function deleteLettersFromWord(wordForDelete){
-	const letter = prompt('Write please letter for delete');
-	for (let i = 0; i <= wordForDelete.length; i++) {
-		if (letter === wordForDelete[i] ) {
-			wordForDelete.splice(i,1);
+function deleteLettersFromWord(word, letter){
+	word = word.split('');
+	for (let i = 0; i <= word.length; i++) {
+		if (letter === word[i] ) {
+			word.splice(i,1);
 		}
 	}
-	return wordForDelete.join('');
+	return word.join('');
 }
-document.writeln(`<p>Result function #9: ${deleteLettersFromWord()}.</p>`);
+document.writeln(`<p>Result function #9: ${deleteLettersFromWord('blablabla','a')}.</p>`);
 
 
 function polyndrom(wordPolyndrom){
+	wordPolyndrom = wordPolyndrom.toLowerCase().replace(/ /g, '').split('');
 	for (let i = 0; i < wordPolyndrom.length; i++) {
 		const letterFromStart = wordPolyndrom[i];
 		const letterFromEnd = wordPolyndrom[wordPolyndrom.length - (i+1)];
@@ -109,7 +101,7 @@ function polyndrom(wordPolyndrom){
 	}
 	return true;
 }
-document.writeln(`<p>Result function #10: ${polyndrom()}.</p>`);
+document.writeln(`<p>Result function #10: ${polyndrom('madam')}.</p>`);
 
 /* як порівнювати всі елементи
 function moreTwoLetter(){
