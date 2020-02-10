fb292327c815d229a99b823e379aa27e773c41af
Таня Бура <tanya.bura.99@gmail.com>
2020-02-10 02:42:53 +0200

    task3

--- homework2/js/homework3.js
+++ homework2/js/homework3.js
@@ -19,7 +19,65 @@ function rowNumber(number, row){
 document.writeln(`<p>Result function #2: ${rowNumber(2,5)}.</p>`);
 
 function changeLetter(){
-	const word = prompt("Write word please:")
-	const change = word.toLowerCase();
-	
+	const word = prompt("Write word please:");
+	return word[0].toUpperCase() + word.slice(1).toLowerCase();		
 }
+document.writeln(`<p>Result function #3: ${changeLetter()}.</p>`);
+
+function salaryWithTax(){
+	const salary = prompt("Write please your salary!");
+	const tax = 19.5;
+	const percent = 100;
+	 return summa = (salary * (percent-tax))/percent;
+}
+document.writeln(`<p>Result function #4: ${salaryWithTax()}.</p>`);
+
+function randomNumber() {
+  min = prompt("Write minimum number");
+  max = prompt("Write maximum number");
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Включаючи мінімум та максимум 
+}
+document.writeln(`<p>Result function #5: ${randomNumber()}.</p>`);
+
+function repeatLetter(){
+	const word = prompt("write word for check").split('');
+	const letter = prompt("Which letter need to check?");
+	let n = 0;
+	for (let i = 0; i <= word.length; i++) {
+		    if (letter === word[i] ) {
+			  n+=1;
+		}
+	}
+return n;
+}
+document.writeln(`<p>Result function #6: ${repeatLetter()}.</p>`);
+
+function currency(){
+	let money = prompt('How much money do you have?').toLowerCase();
+	const dollar = 25;
+	
+	if (money.indexOf('$') + 1) {
+
+	let summa = money.slice(0, -1) * dollar;
+	console.log(summa)
+		} else {
+	summa = money/dollar;
+		}
+return summa;
+}
+document.writeln(`<p>Result function #7: ${currency()}.</p>`);
+
+function password(){
+	let lengthPassword = prompt('Write to length for your password');
+	let arrForNumber = [];
+	console.log(lengthPassword);
+			if(lengthPassword === ''){
+				 lengthPassword = 8;
+			}
+		for(let i=0; i<lengthPassword;i++){
+			const randomNumber = Math.random()*10;
+			arrForNumber.push(Math.round(randomNumber));
+		}
+	return arrForNumber.join('');
+}
+document.writeln(`<p>Result function #8: ${password()}.</p>`);
