0826fb2f59099b842637ef3e8241ca68c9da7b53
Таня Бура <tanya.bura.99@gmail.com>
2020-02-19 21:33:33 +0200

    task4

--- HW_All/js/homework4.js
+++ HW_All/js/homework4.js
@@ -1,73 +1,82 @@
 const students = ["Sasha", "Igor","Oleksiy" , "Ira", "Lena", "Svitlana"];
 const themes = ["Differential equations", "Theory of Automata", "Algorithms and data structures"];
 const marks = [4, 5, 5, 3, 4, 5];
-let couple = [];
-function coupleFriends(){
-	let arr = [];
+function coupleFriends(names){
+	const couple = [];
+	const arr = [];
 	let k = 0;
-	for(let i = 0; i < students.length/2;i++){
-		arr[k] = [students[i],students[i + students.length/2]];
+	for(let i = 0; i < names.length/2;i++){
+		arr[k] = [names[i],names[i + names.length/2]];
 		couple.push(arr[k]);
 		k++;
 	}
 	return couple;
 }
-console.log(coupleFriends());
+console.log(coupleFriends(students));
+const couples = coupleFriends(students);
 document.writeln('<h3>Function #1:<h3>')
-document.writeln(`<p>First couple: ${couple[0]}.</p>`);
-document.writeln(`<p>Second couple: ${couple[1]}.</p>`);
-document.writeln(`<p>Third couple: ${couple[2]}.</p>`);
+for(let j = 0; j < couples.length;j++ ){
+	document.writeln(`<p>Couple: ${couples[j]}.</p>`);
+}
 
-let project = [];
-function projectSciennce(){
-	const stringOne = couple[0].join(' and ');
-	const stringTwo = couple[1].join(' and ');
-	const stringThree = couple[2].join(' and ');
-	let names = [];
-	names.push(stringOne,stringTwo,stringThree);
-	for( let i = 0; i < themes.length; i++){
-		project.push([names[i],themes[i]]);
+function projectSciennce(couple,theme){
+	const project = [];
+	const names = [];
+	for(let i=0;i<couple.length; i++){
+		names.push(couple[i].join(' and '));
+	}
+	for( let j = 0; j < theme.length; j++){
+		project.push([names[j],theme[j]]);
 	}
 	return project;
 }
-console.log(projectSciennce());
+console.log(projectSciennce(couples,themes));
+const namesAndThemes = projectSciennce(couples,themes);
 document.writeln('<h3>Function #2:<h3>')
-document.writeln(`<p>First project: ${project[0]}.</p>`);
-document.writeln(`<p>Second project: ${project[1]}.</p>`);
-document.writeln(`<p>Third project: ${project[2]}.</p>`);
+for(let i = 0; i<namesAndThemes.length; i++){
+	document.writeln(`<li>Project: ${namesAndThemes[i]}.</li>`);
+}
 
-let markStudent = [];
-function markForStudent(){	
-	for( let i = 0; i < students.length; i++){
-		markStudent.push([students[i],marks[i]]);
+
+function markForStudent(names,mark){	
+	const markStudent = [];
+	for( let i = 0; i < names.length; i++){
+		markStudent.push([names[i],mark[i]]);
 	}
 	return markStudent;
 }
-console.log(markForStudent());
-document.writeln('<h3>Function #3:<h3>')
-document.writeln('<p>Marks:<p>');
-document.writeln(`<li> ${markStudent[0]}.</li>`);
-document.writeln(`<li> ${markStudent[1]}.</li>`);
-document.writeln(`<li> ${markStudent[2]}.</li>`);
-document.writeln(`<li> ${markStudent[3]}.</li>`);
-document.writeln(`<li> ${markStudent[4]}.</li>`);
-document.writeln(`<li> ${markStudent[5]}.</li>`);
+console.log(markForStudent(students,marks));
 
-let markForProject = [];
-function randomMarksForProject(){
-	projectSciennce();
+const studentWithMark = markForStudent(students,marks);
+
+document.writeln('<h3>Function #3:<h3>');
+document.writeln('<p>Marks:<p>');
+for(let i = 0; i<studentWithMark.length; i++){
+	document.writeln(`<li>${studentWithMark[i]}.</li>`);
+}
+
+function randomMark(arr){
+	const arrayMark = [];
 	const maxMark = 5;
-	let arrayMark = [];
-	arrayMark.push(Math.floor(Math.random()*maxMark), Math.floor(Math.random()*maxMark),Math.floor(Math.random()*maxMark));
-	console.log(arrayMark);
-	for( let i = 0; i < 3; i++){
-		project[i].push(arrayMark[i]);
-		markForProject.push([project[i]]);
+	const minMark = 1;
+	for(let i=0; i<arr.length;i++){
+		arrayMark.push(Math.floor(Math.random() * (maxMark - minMark) + minMark));
+	}
+	return arrayMark;
+}
+console.log(randomMark(namesAndThemes));
+const randMark = randomMark(namesAndThemes);
+function randomMarksForProject(student){
+	const markForProject = [];
+	for( let i = 0; i < randMark.length; i++){
+		student[i].push(randMark[i]);
+		markForProject.push([student[i]]);
 	}
 	return markForProject;
 }
-console.log(randomMarksForProject());
-document.writeln('<h3>Function #4:<h3>')
-document.writeln(`<p>Project with mark: ${markForProject[0]}.</p>`);
-document.writeln(`<p>Project with mark: ${markForProject[1]}.</p>`);
-document.writeln(`<p>Project with mark: ${markForProject[2]}.</p>`);
+const allProject = randomMarksForProject(namesAndThemes);
+console.log(allProject);
+document.writeln('<h3>Function #4:<h3>');
+for(let i = 0; i<allProject.length; i++){
+	document.writeln(`<p>Project: ${allProject[i]}.</p>`);
+}
