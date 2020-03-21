2b3d986e95f39024b585aada1ec8870a0db33b3d
Таня Бура <tanya.bura.99@gmail.com>
2020-03-21 15:31:09 +0200

    task12

--- HW_All/js/homework12.js
+++ HW_All/js/homework12.js
@@ -1,11 +1,63 @@
+const avatars = ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg','images/10.jpg','images/11.png','images/12.jpg','images/13.jpg','images/14.jpg','images/15.png','images/16.jpg'];
+const butt = document.getElementById('getInf');
+const find = document.getElementById('number');
+const container = document.querySelector('.characters');
 const planetButton = document.getElementById('getPlanet');
+let page = 2;
+async function getCharacters(id){
+	const res = await axios.get(`https://swapi.co/api/films/${id}/`);
+	return res.data.characters;
+}
 async function getPlanets(id){
 	const res = await axios.get(`https://swapi.co/api/planets/?page=${id}`);
 	console.log(res.data.results);
 	return res.data.results;
 }
+async function renderCharactersFiveEpisod(id){
+	const people = await getCharacters(`${id}`);
+	console.log(people);
+	for(let i = 0; i < people.length; i++){
+		async function createCard(){
+			const res = await axios.get(`${people[i]}`);
+			const personDiv = document.createElement('div');
+			personDiv.classList.add('person');
+			personDiv.insertAdjacentHTML('beforeend', `
+				<img src=${avatars[i]}>
+				<p>${res.data.name}</p>
+				<h3>${res.data.birth_year}</h3>
+				<h6>${res.data.gender}</h6>`);
+			container.append(personDiv);
+		}
+		createCard();
+	}
+}
+async function renderCharactersAllEpisod(id){
+	const people = await getCharacters(`${id}`);
+	people.forEach(async(link)=> {
+		const res = await axios.get(`${link}`);
+		const personDiv = document.createElement('div');
+		personDiv.classList.add('character');
+		personDiv.innerHTML = `
+		<p>${res.data.name}</p>
+		<h3>${res.data.birth_year}</h3>
+		<h6>${res.data.gender}</h6>`
+		container.append(personDiv);
+	});
+}
+butt.addEventListener('click',() =>{
+	const numberEpisod = find.value;
+	if(numberEpisod == 2){
+		container.innerHTML = '';
+		renderCharactersFiveEpisod(numberEpisod);
+		find.value = '';
+	} else {
+		container.innerHTML = '';
+		renderCharactersAllEpisod(numberEpisod);
+		find.value = '';
+	}
+});
 async function renderPlanets(id){
-	const container = document.querySelector('.characters');
+	container.innerHTML = '';
 	const planets = await getPlanets(`${id}`);
 	planets.forEach((planet)=> {
 		const planetDiv = document.createElement('div');
@@ -13,30 +65,16 @@ async function renderPlanets(id){
 		planetDiv.innerHTML = `<h1>${planet.name}</h1>`
 		container.append(planetDiv);
 	});
+	page++;
+	if(page == 8){
+		next.parentNode.removeChild(next);
+	} 
 }
-
 planetButton.addEventListener('click',async()=>{
 	await renderPlanets(1);
 	planetButton.insertAdjacentHTML('afterend', `<button class = 'inform' id = 'next'>Next</button>`);
 	const next = document.getElementById('next');
-	next.addEventListener('click',async()=>{
-		const planets = document.querySelector('.characters');
-		const div = document.querySelectorAll('.planet');
-		await div.forEach((div) =>{
-			planets.removeChild(div);
-		});
-		renderPlanets(2);
-		next.parentNode.removeChild(next);
-		planetButton.insertAdjacentHTML('afterend', `<button class = 'inform' id = 'nextTwo'>Next</button>`);
-		const nextTwo = document.getElementById('nextTwo');
-		console.log(nextTwo);
-		nextTwo.addEventListener('click',async()=>{
-			const planets = document.querySelector('.characters');
-			const div = document.querySelectorAll('.planet');
-			div.forEach((div)=>{
-				planets.removeChild(div);
-			});
-			renderPlanets(3);
-		});
-	});
+	next.addEventListener('click',()=>{
+		renderPlanets(page);
+	});	
 });
